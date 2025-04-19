44ae7ec79eadf3e9b7b81e34f5203c3fb7096bb2
TheBattleDog <krishnanand7777777@gmail.com>
2025-04-19 19:13:26 +0530

    Commands added

--- foldersize.bat
+++ foldersize.bat
@@ -1,2 +1,3 @@
+REM foldersize command that will list the size of folders
 @echo off
 powershell -NoExit -File "C:\Scripts\foldersize.ps1"
--- zed.bat
+++ zed.bat
@@ -1,2 +1,3 @@
+REM zed command used to open zed-editor.exe in pwd
 @echo off
 start "" zed-editor.exe "%CD%"
